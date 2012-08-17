609961da8164a703b4cd7d53ce3bc836b0774939
artem <vovk.artem@gmail.com>
2012-08-17 23:43:44 +0200

    Initial commit

---
+++ LANotificationKitDemo/LANotificationKitDemo/LAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LAAppDelegate.h
+//  LANotificationKitDemo
+//
+//  Created by Artem Vovk on 8/17/12.
+//  Copyright (c) 2012 LessApps. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface LAAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
